4baad28bc3c359a3684f1fd50133e17ee039c604
wavejumper3 <calculatorcoder42@gmail.com>
2026-01-24 16:19:25 -0500

    Did the thing

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+current_data = {
+	"temperature": 0,
+	"humidity": 0
+}
+
 @app.route('/')
 def index():
     return render_template("index.html")
