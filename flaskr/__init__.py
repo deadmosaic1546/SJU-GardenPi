a57fdda6102364d2dd8870d67ce9c07fc40d6fb4
wavejumper3 <calculatorcoder42@gmail.com>
2026-01-31 14:49:38 -0500

    made it so the beds are seperate, added the data fields properly

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-current_data = {
-	"temperature": 0,
-	"humidity": 0
-}
+bed_data = [
+	"dummy data that makes it 1-indexed",
+	{
+		"temperature": -5,
+		"humidity": 0
+	},
+	{
+		"temperature": 5,
+		"humidity": 10
+	},
+	{
+		"temperature": 15,
+		"humidity": 20
+	},
+	{
+		"temperature": 25,
+		"humidity": 30
+	}
+	]
 
 @app.route('/')
 def index():
-    return render_template("index.html", current_data=current_data)
+    return render_template("index.html", bed=bed_data)
